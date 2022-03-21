eee9736e72ecf5f9fb08d44df02fc22ab0fc659c
guiparpineli <guiparpineli@pop-os.localdomain>
2022-03-21 20:03:01 -0300

    Js feito

--- main.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("h1").style.color = 'black'
-document.querySelector("h1").style.textAlign = 'center'
-
-document.querySelector("body").style.textAlign = 'center'
-document.querySelector("div1").style.textAlign= 'center'
-
-
-
