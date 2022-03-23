e178281fd4954b4e23f654a81ab88958f9ce49b1
Karina Bueno <buenogkarina@gmail.com>
2022-03-23 12:05:47 -0300

    Fonte Atualizada

---
+++ Documents/DOCS_KARINA/Node/Front-End-II/aula08/script.js
@@ -0,0 +1,25 @@
+//let hiperlink = document.querySelector('a');
+
+//hiperlink.addEventListener('click', function (event) {
+   // console.log('Você clicou');
+
+    //event.preventDefault();
+//})
+
+    alert("Eu sou um alert!")
+       
+    function mudaParaAmarelo(td) {
+        td.style.backgroundColor = "#FFFA00";
+    }
+    
+    function mudaParaVerde(td) {
+        td.style.backgroundColor = "#31FF4E";
+    }
+
+    if (){
+        mudaParaVermelho(td);
+    }
+    else if (quantidadeDeDias < 15){
+        mudaParaAmarelo(td);
+    }
+
---
+++ Documents/DOCS_KARINA/Node/Front-End-II/checkpoint1/main.js
@@ -0,0 +1,8 @@
+document.querySelector("h1").style.color = 'black'
+document.querySelector("h1").style.textAlign = 'center'
+
+document.querySelector("body").style.textAlign = 'center'
+document.querySelector("div1").style.textAlign= 'center'
+
+
+
